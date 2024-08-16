e98334151868a4fef807129b4e7a32dba997e6d0
nestorustariz <70995521+nestorustariz@users.noreply.github.com>
2024-08-16 02:07:52 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Mensaje de bienvenida al cargar la página
+window.onload = function() {
+    alert('¡Bienvenido! Nos encantaría que nos acompañes en este día tan especial.');
+};
+
+function confirmarAsistencia() {
+    alert('¡Gracias por confirmar tu asistencia!');
+}
